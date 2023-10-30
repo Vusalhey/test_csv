c82f75214974acb2a2621881d0978775978586ef
Vusalhey <vusal.tot2017@yandex.ru>
2023-10-30 15:15:34 +0300

    testing for push

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import _ from 'lodash';
 
-export default function solution(content){
+export default function solution(content) {
   // BEGIN
 
-  //step1
+  // перви шак
   const formatting = content.trim().split('\n').slice(0, -1);
   const result1 = formatting.map((el) => el.slice(0, -1).split(','));
-  console.log(`Число пассажиров: ${result1.length}`);
+  console.log(`Число пассажиров: ${result1.length}`);cd 
+
 
   //step2
   const findEmbar = result1.map((n) => n[12]);
@@ -28,20 +29,20 @@ export default function solution(content){
 
   console.log(`процент выживших пассажиров: ${calcSurv}%`);
 
-  //step5 
-  //const namesPassengers = result1.map((n) => n.toString().split(',')[4]);
-  //const result5 = namesPassengers.map((qq) => qq[1] === 'A');
-  //console.log(`имена всех пассажиров, начинающиеся на букву А: ${result5}`)
-// stepp
-// const formating - content.split('\n').slice(0, -1);
-// const result1 = formating.map((el) => el.slice(0, -1).split('| '));
-//console.log(`Число писей: ${result1.length}`)
-//stepp
-// const format = content.split('\n').slice(1, -1);
-// const newResult = format.map((el) => el.slice(1, -2).split(' | '));
-// const sunCityTier = newResult.map((el) => el[4]);
-// const uniqCities = _.uniq(subCityTier).sort();
-// console.log(`Список: ${uniqCities.join(', ')}`);
+  step5 
+  const namesPassengers = result1.map((n) => n.toString().split(',')[4]);
+  const result5 = namesPassengers.map((qq) => qq[1] === 'A');
+  console.log(`имена всех пассажиров, начинающиеся на букву А: ${result5}`)
+stepp
+const formating = content.split('\n').slice(0, -1);
+const result1 = formating.map((el) => el.slice(0, -1).split('| '));
+console.log(`Число писей: ${result1.length}`)
+stepp
+const format = content.split('\n').slice(1, -1);
+const newResult = format.map((el) => el.slice(1, -2).split(' | '));
+const sunCityTier = newResult.map((el) => el[4]);
+const uniqCities = _.uniq(subCityTier).sort();
+console.log(`Список: ${uniqCities.join(', ')}`);
 // stepp 
 // const place = newResult.flatmap((el) => _.camelCase(el[2]));
 // const maxPlace = Math.max(...place);
